e220cd3ccad3b7d77e53d38dfdf17ad203e6962f
microsoftbob <40544061+microsoftbob@users.noreply.github.com>
2021-10-18 10:30:39 +0100

    Initial Commit of R Code

---
+++ CHRO Openness.R
@@ -0,0 +1,31 @@
+#For Metrics Project 1 
+
+#Pull in the data from the correct spreadsheet 
+
+chro <- read_excel(file.choose())
+chro
+
+
+#Summarize information about the data in general. 
+
+describe(chro)
+
+#Chronbach's Alpha 
+
+cronbach(chro[,c("NBC1a", "NBC1c", "NBC1g", "NBC1kr")])
+item.total(chro[,c("NBC1a", "NBC1c", "NBC1g", "NBC1kr")])
+
+
+#Create Column for Leadership Team Openness
+
+chro$openness.S<-rowMeans(chro[,c("NBC1a", "NBC1c", "NBC1g", "NBC1kr")],na.rm=TRUE)
+histogram(chro$openness.S)
+
+describe(chro$openness.S)
+
+#To find info about Firm 49 
+
+chro$openness.S[chro$FirmID==49]
+
+#This tells me which firms are below 2 std 
+chro$FirmID[chro$openness.S<2.2&!is.na(chro$openness.S)]
