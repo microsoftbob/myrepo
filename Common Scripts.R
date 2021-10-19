5f08c54587350e318e7ba00fe6a66756a52adbf7
microsoftbob <40544061+microsoftbob@users.noreply.github.com>
2021-10-19 18:42:42 +0100

    Basic Updates

--- Common Scripts.R
+++ Common Scripts.R
@@ -1,20 +1,19 @@
-####Set Working Directory####
-
-setwd("C:/Users/romann/One-Drive - Microsoft/Desktop/R")
-source("libraries.R")
-
-
-####Clear Workspace####
-
+#### Clear Workspace ####
 rm(list = ls())
 
-####Read Excel####
+#### Read Excel ####
 library(readxl)
 table1 <- read_excel(file.choose())
 
+#### Summarize information about the data in general #### 
+library(psych)
+describe(table1)
+summary(table1)
 
+#### Change Column Name ####
+table1$`column name` <- NULL
 
-
-
+#### Change Column Data Type ####
+table1$'Stock Level' <- as.numeric(table1$'Stock Level')
 
 
